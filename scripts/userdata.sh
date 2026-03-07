f4d03403e3fce622e9a20ed9b18c2207dcd88937
Ailsa Hogben <ailsah@comcast.net>
2026-03-06 20:13:41 -0800

    fix userdata

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,12 +1,21 @@
 #!/bin/bash -xe
+
 yum update -y
 
 # Install Docker
 yum install -y docker
 
-# Start Docker
 systemctl start docker
 systemctl enable docker
 
-# Allow ec2-user to run Docker
 usermod -aG docker ec2-user
+
+# Install Docker Compose
+curl -L https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 \
+-o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+# Verify installation
+docker --version
+/usr/local/bin/docker-compose --version
