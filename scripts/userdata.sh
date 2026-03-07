dd4009bd8c824d56803a93c1a649c631d79165ea
Ailsa Hogben <ailsah@comcast.net>
2026-03-06 19:57:21 -0800

    remove docker-compose from userdata

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -10,9 +10,3 @@ systemctl enable docker
 
 # Allow ec2-user to run Docker
 usermod -aG docker ec2-user
-
-# Install docker-compose
-curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) \
--o /usr/local/bin/docker-compose
-
-chmod +x /usr/local/bin/docker-compose
