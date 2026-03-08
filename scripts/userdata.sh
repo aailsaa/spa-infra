f501b845ed158c739265ab11e61e870f4722f5f9
Ailsa Hogben <152041858+aailsaa@users.noreply.github.com>
2026-03-08 04:19:59 -0700

    remove start docker from userdata

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,21 +1,10 @@
 #!/bin/bash 
 set -xe
 
-# Install Docker
-echo "Installing Docker..."
-yum install -y docker
-
-echo "Installing Docker..."
+# Run docker (backup of AMI)
+echo "Running Docker..."
 systemctl start docker
 systemctl enable docker
-usermod -aG docker ec2-user
-
-# Install Docker Compose
-curl -L https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 \
--o /usr/local/bin/docker-compose
-
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 docker --version
 docker-compose --version
