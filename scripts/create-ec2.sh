16b5745ce4ed9e4bd33581c559eac7d2ea1ef716
Ailsa Hogben <152041858+aailsaa@users.noreply.github.com>
2026-03-08 01:48:19 -0800

    Update create-ec2.sh

--- scripts/create-ec2.sh
+++ scripts/create-ec2.sh
@@ -4,23 +4,25 @@ set -e
 echo "Launching temporary EC2..."
 
 INSTANCE_ID=$(aws ec2 run-instances \
-  --image-id ami-0c02fb55956c7d316 \
+  --image-id $AMI_ID \
   --instance-type t2.micro \
   --security-group-ids $EC2_SECURITY_GROUP \
   --subnet-id $EC2_SUBNET \
+  --associate-public-ip-address \
+  --key-name $EC2_KEY_NAME \
+  --user-data file://scripts/userdata.sh \
   --query 'Instances[0].InstanceId' \
   --output text)
 
-echo "Instance ID: $INSTANCE_ID"
-
-echo "Waiting for EC2 to start..."
+echo "INSTANCE_ID=$INSTANCE_ID" >> $GITHUB_ENV
 
 aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
-IP=$(aws ec2 describe-instances \
+EC2_IP=$(aws ec2 describe-instances \
   --instance-ids $INSTANCE_ID \
   --query 'Reservations[0].Instances[0].PublicIpAddress' \
   --output text)
 
-echo "EC2_IP=$IP" >> $GITHUB_ENV
-echo "INSTANCE_ID=$INSTANCE_ID" >> $GITHUB_ENV
+echo "EC2_IP=$EC2_IP" >> $GITHUB_ENV
+
+echo "Temporary EC2 IP: $EC2_IP"
